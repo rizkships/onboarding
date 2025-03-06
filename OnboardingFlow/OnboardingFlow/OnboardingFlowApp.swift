06d80184feb5f70fa893440c6c114ad86ac52580
rizk <rizkdotai@gmail.com>
2025-03-06 23:56:09 +0200

    add onboarding struct

---
+++ OnboardingFlow/OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by Omar Rizk on 3/6/25.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OnboardingFlow/OnboardingFlowTests/OnboardingFlowTests.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowTests.swift
+//  OnboardingFlowTests
+//
+//  Created by Omar Rizk on 3/6/25.
+//
+
+import Testing
+@testable import OnboardingFlow
+
+struct OnboardingFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
